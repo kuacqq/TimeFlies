226c7d52877aa91675fc162fc9aa6a4f1717d1d7
Tristan Whang <twhang@usc.edu>
2022-04-08 20:20:10 -0700

    Added singleton and many different versions of addcoords to accomodate different potential data types

--- WhangTristan_FinalProject/Controller/MapViewController.swift
+++ WhangTristan_FinalProject/Controller/MapViewController.swift
@@ -6,15 +6,52 @@
 //
 
 import UIKit
+import CoreLocation
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let route = RouteModel.shared
+    let coordinateArray = RouteModel.shared.coordinatesArray
+    let locationArray = RouteModel.shared.locationArray
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
     
+    /*
+     these are some really important methods that you will always use
+     */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // optional binding
+        if let location = locations.first {
+            // there is a lot you can access from CLLocation, like time, altitude, literally
+            // everything. You will need it for your project
+//            print("user location : lat \(location.coordinate.latitude), lng \(location.coordinate.longitude)")
+            route.addCorrdinate(coords: location.coordinate)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("\(#function) \(error.localizedDescription)")
+    }
+    
+    /*
+     this is the method that is called when the location authorization
+     changes
+     */
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print("\(#function)")
+        if (manager.authorizationStatus == .authorizedAlways) {
+            
+        }
+    }
 
     /*
     // MARK: - Navigation
--- WhangTristan_FinalProject/Model/RouteModel.swift
+++ WhangTristan_FinalProject/Model/RouteModel.swift
@@ -10,7 +10,7 @@ import CoreLocation
 
 class RouteModel: NSObject, RouteDataModel {
     
-    
+    static let shared = RouteModel()
     var locationArray: [Location]
     var coordinatesArray: [CLLocationCoordinate2D]
     var date: Date
@@ -25,8 +25,21 @@ class RouteModel: NSObject, RouteDataModel {
         let pin: Location = Location(lng, lat, hrsSpent, minSpent)
         locationArray.append(pin)
     }
+    
+    // this is so i can use
+    // let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+    // mapView.addOverlay(polyline)
     func addCorrdinate(coords: CLLocationCoordinate2D) {
         coordinatesArray.append(coords)
+    }
+    func addCoordinate(lng: Int, lat: Int) {
+        let degLongitude: CLLocationDegrees = CLLocationDegrees(lng)
+        let degLatitude: CLLocationDegrees = CLLocationDegrees(lat)
+        let coords = CLLocationCoordinate2D(latitude: degLongitude, longitude: degLatitude)
+        coordinatesArray.append(coords)
+    }
+    func addCoorinates(lng: CLLocationDegrees, lat: CLLocationDegrees) {
+        coordinatesArray.append(CLLocationCoordinate2D(latitude: lat, longitude: lng))
     }
     
     
